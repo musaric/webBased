818a3c1f5433867084520bb9310433acbb379075
musaric <carlos220594l@gmail.com>
2016-11-28 22:59:16 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+session_start()
+
+
+
+?>
+
+
+
+<html>
+<!-- Here we display the content of the mainpage of the webpage -->
+	<head><title>Main page</title>
+		<link rel="stylesheet" type="text/css" href="pStyles.css">
+
+	</head>
+
+	<body>
+		<header>
+		<ul class="topMenu">
+			<li > <a  href="mainpage.php">Main page</a></li>
+			<li > <a  href="login.php">login</a></li>
+			<li > <a  href="registar.php">Registration</a></li>
+		</ul>
+</header>
+
+
+
+
+
+
+<!--
+    <ul >
+			<li style="display: inline-block; padding: 14px 16px;"> <a style=" text-decoration: none;" href="mainpage.php">Main page</a></li>
+			<li style="display: inline-block; padding: 14px 16px;"> <a style=" text-decoration: none;" href="login.php">login</a></li>
+			<li style="display: inline-block; padding: 14px 16px;"> <a style=" text-decoration: none;" href="signUp.php">Registration</a></li>
+			<li style="display: inline-block; padding: 14px 16px;"> <a style=" text-decoration: none;" href="modify.php">User</a></li>
+		</ul>
+-->
+
+
+
+	</body>
+
+</html>
